e94723747105722f683362bc6e4a6295dd303799
rohit singh <rohitsingh17172000@gmail.com>
2020-08-31 22:25:32 +0530

    company

--- bill/models.py
+++ bill/models.py
@@ -6,6 +6,6 @@ payment = [(1,'Cash'),(2,'Credit')]
 class Bill_Wholeseller(models.Model):
     date = models.DateField(default=timezone.now,null=False)
     mode_of_payment = models.PositiveSmallIntegerField(choices=payment,default=1)
-    
+    #products
 
 
