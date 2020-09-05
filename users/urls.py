743aef6a796af1d9073222e2b6970119432c9af3
Richesh Gupta <rgrichesh45@gmail.com>
2020-09-05 20:14:04 +0530

    basic Signup page

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . views import *
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     path('',home,name='home'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('test/',Test,name='test'),
-    path('reg/',Reg,name="register"),
+    path('reg/',Signup,name="register"),
     path('logout/',Logout,name='logout'),
-]
+
+    ]
