34962d0d6df35b2252479246cddacf37467af9fc
satyam260 <satyamupadhyay260@gmail.com>
2020-09-08 11:42:38 +0530

    Added login required for sale

--- bill/views.py
+++ bill/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from .models import Bill_Retailer
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='/')
 def Sale(request):
     return render(request,"bill/sale.html",{})
-
 
 def create_bill_sale(request):
     if request.method == 'POST':
