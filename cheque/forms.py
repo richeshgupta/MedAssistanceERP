3a6deca5b8ca11f16d06d09e8bb82a20529c6aea
satyam260 <satyamupadhyay260@gmail.com>
2020-09-14 12:37:30 +0530

    Changes in home page and cheque form

--- cheque/forms.py
+++ cheque/forms.py
@@ -3,6 +3,14 @@ from django import forms
 from .models import *
 
 class ChequeForm(ModelForm):
+
     class Meta:
         model = Cheque
         fields = '__all__' 
+
+        widgets = {
+            'cheque_num' : forms.TextInput(attrs={'placeholder': 'Cheque Number'}),
+            'date_issued' : forms.DateInput(attrs={'placeholder': 'YYYY-MM-DD'}),
+            'date_for_assigned' : forms.DateInput(attrs={'placeholder': 'YYYY-MM-DD'}),
+            'amount' : forms.NumberInput(attrs={'placeholder': 'Amount'}),
+        }
