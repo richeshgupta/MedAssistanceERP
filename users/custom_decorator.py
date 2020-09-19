b30a6798497cc2cda06d17b21e6d7e9659349ad3
Richesh Gupta <rgrichesh45@gmail.com>
2020-09-19 20:44:14 +0530

    intermediate access decorator added

--- users/custom_decorator.py
+++ users/custom_decorator.py
@@ -21,3 +21,21 @@ def is_admin_access(func):
             msg = "You are not signed in"
             return render(request,"users/error.html",{'error':msg})
     return _wrapper_
+
+def is_intermediate_access(func):
+    def _wrapper_(request,*args,**kwargs):
+
+        if not request.user.is_anonymous:
+            try:
+                xuser = user_extended.objects.get(user=request.user)
+                if xuser.access_level != 2 and xuser!=None:
+                    return render(request,"users/error.html",{'error':"You don't have access to this page"})
+                return func(request,*args,**kwargs)
+            except:
+                msg = "Some Error Occured, May be Your access Level is not updated, Kindly Contact Admin"
+                return render(request,"users/error.html",{'error':msg})
+
+        else:
+            msg = "You are not signed in"
+            return render(request,"users/error.html",{'error':msg})
+    return _wrapper_
