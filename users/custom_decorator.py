13b094b83013a2120b510e7f7acc0eae80de0bd6
Richesh Gupta <rgrichesh45@gmail.com>
2020-09-23 23:51:46 +0530

    Password change module changes

--- users/custom_decorator.py
+++ users/custom_decorator.py
@@ -7,12 +7,12 @@ from django.shortcuts import render
 def is_admin_access(func):
     def _wrapper_(request,*args,**kwargs):
 
-        if not request.user.is_anonymous:
-            try:
+        if not request.user.is_anonymous: #checking if user is logged in or not
+            try: #try if the user even has data in user_extended model
                 xuser = user_extended.objects.get(user=request.user)
-                if xuser.access_level != 3 and xuser!=None:
+                if xuser.access_level != 3 and xuser!=None: #if user has admin access
                     return render(request,"users/error.html",{'error':"You don't have access to this page"})
-                return func(request,*args,**kwargs)
+                return func(request,*args,**kwargs) #basic syntax for decorator also args and kwargs help to handle multiple args passing without error
             except:
                 msg = "Some Error Occured, Maybe Your access Level is not updated, Kindly Contact Admin"
                 return render(request,"users/error.html",{'error':msg})
@@ -21,6 +21,8 @@ def is_admin_access(func):
             msg = "You are not signed in"
             return render(request,"users/error.html",{'error':msg})
     return _wrapper_
+
+
 
 def is_intermediate_access(func):
     def _wrapper_(request,*args,**kwargs):
