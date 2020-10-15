c459074544988e2cee6ae6c0002ab15b87e2f75f
rohit singh <rohitsingh17172000@gmail.com>
2020-10-15 12:05:05 +0530

    add batch completed

--- company/urls.py
+++ company/urls.py
@@ -3,11 +3,13 @@ from .views import *
 
 urlpatterns = [
     path('company/',Company,name='company'),
-    path('product/',Product,name='product'),
     path('product2/',Product2,name='product'),
-    path('batch/',Batch,name='batch'),
+    path('batch/',Batch1,name='batch'),
     path('product2/getMedCompanies/',GetMedCompanies),
     path('product2/addProduct/',AddProduct),
     path('product2/getCompanyID/',GetCompanyID),
-    
+    path('batch/getMedProducts/',GetMedProducts),
+    path('batch/getMedCompany/',GetMedCompany),
+    path('batch/getMedID/',GetMedID),
+    path('batch/addBatch/',AddBatch),
 ]
--- company/views.py
+++ company/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.db import connection
 from django.http import HttpResponse,JsonResponse
 import json
-from company.models import Product
+from company.models import Product,Batch
 
 
 @login_required(login_url='home')
@@ -45,28 +45,35 @@ def Company(request):
 #     return render(request,'company/product.html',context)
 
 
-@login_required(login_url='home')
-def Batch(request):
-    form = batchForm()
-    if request.method == 'POST':
-        form = batchForm(request.POST)
-        if form.is_valid():
-            form.save()
-            if 'next' in request.POST:
-                return redirect(request.POST.get('next'))
-            else:
-                return redirect('batch')
-        else:
-            return redirect('batch')
-    else:
-        form = batchForm()
-    context = {'form':form}
-    return render(request,'company/batch.html',context)
+# @login_required(login_url='home')
+# def Batch(request):
+#     form = batchForm()
+#     if request.method == 'POST':
+#         form = batchForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             if 'next' in request.POST:
+#                 return redirect(request.POST.get('next'))
+#             else:
+#                 return redirect('batch')
+#         else:
+#             return redirect('batch')
+#     else:
+#         form = batchForm()
+#     context = {'form':form}
+#     return render(request,'company/batch.html',context)
 
+
+#dont change the name to product
 @login_required(login_url='home')
 def Product2(request):
     context={}
     return render(request,"company/product2.html",context)
+
+@login_required(login_url='home')
+def Batch1(request):
+    context={}
+    return render(request,"company/batch.html",context)
 
 
 def GetMedCompanies(request):
@@ -105,5 +112,59 @@ def AddProduct(request):
             sale_rate = request.POST['sale_rate'],
             gst = request.POST['gst'],
             free = 0,
+        )
+        return HttpResponse('')
+
+def GetMedProducts(request):
+    if request.method=="GET":
+        cursor = connection.cursor()
+        cursor.execute("SELECT name FROM company_product")
+        data= cursor.fetchall()
+        b=[]
+        [b.append(a[0]) for a in data if a[0] not in b]
+        return HttpResponse(json.dumps(b), content_type='application/json')
+
+def GetMedCompany(request):
+    if request.method=="GET":
+        medName=request.GET['medName']
+        cursor = connection.cursor()
+        cursor.execute("SELECT company_id FROM company_product where name=%s",[medName])
+        data= cursor.fetchall()
+        b=[a[0] for a in data]
+        d=[]
+        for a in b:
+            cursor.execute("SELECT comp_name FROM company_company where id=%s",[a])
+            d.append(cursor.fetchone()[0])
+        return HttpResponse(json.dumps(d), content_type='application/json')
+
+def GetMedID(request):
+    if request.method == "GET":
+        medName = request.GET['MedName']
+        compName = request.GET['CompanyName']
+        cursor = connection.cursor()
+        cursor.execute("SELECT id FROM company_company where comp_name=%s",[compName])
+        medCompany=cursor.fetchone()[0]
+        cursor.execute("SELECT id FROM company_product where name=%s and company_id=%s",[medName,medCompany])
+        return HttpResponse(json.dumps(cursor.fetchone()), content_type='application/json')
+    else:
+        return ErrorPage(request,"Only GET allowed")
+
+def AddBatch(request):
+    if request.method == 'POST':
+        cursor = connection.cursor()
+        cursor.execute("SELECT max(id) FROM company_batch")
+        a=cursor.fetchone()[0]
+        if(isinstance(a, int)):
+            a=a+1
+        else:
+            a=1
+        Batch.objects.create(
+            id = a,
+            batch_number = request.POST['batch_number'],
+            expiry = request.POST['expiry'],
+            product_id = int(request.POST['product_id']),
+            mrp = request.POST['mrp'],
+            purchase_rate = request.POST['purchase_rate'],
+            quantity = 0,
         )
         return HttpResponse('')
