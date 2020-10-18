eecae5880063c4a9b1927000411942822ac07894
satyam260 <satyamupadhyay260@gmail.com>
2020-10-18 20:33:52 +0530

    Report Section

--- reports/urls.py
+++ reports/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('report/',report,name='report'),
+    path('saleReportDuration/',report,name='saleReportDuration'),
+    path('saleReportCompany/',report,name='saleReportCompany'),
+    path('saleReportDate/',report,name='saleReportDate'),
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 def report(request):
+    if request.method == 'POST':
+        duration = request.POST.get('time')
+        print(duration)
     return render(request,'reports/report.html',{})
 
