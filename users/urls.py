9503eaf21170c1cdcee4878819d242376db62e49
Richesh Gupta <rgrichesh45@gmail.com>
2020-10-25 13:52:32 +0530

    UI fixes

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    # path('',home,name='home'),
+    
     path('',auth_views.LoginView.as_view(template_name='users/index.html'),name='home'),
     path('test/',Test,name='test'),
     path('reg/',Signup,name="register"),
