409397e2dcb91ab9ce20b294d3084100ec31ce3d
Richesh Gupta <rgrichesh45@gmail.com>
2020-11-13 14:02:05 +0530

    GSTR completed

--- gst/urls.py
+++ gst/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import GSTGenerate,GSTSaleReturn,GSTPurchaseReturn,GSTSaleReport
+from .views import *
 urlpatterns = [
     path('',GSTGenerate,name="gst"),
     path('saleReturn/',GSTSaleReturn,name="saleReturn"),
     path('PurchaseReturn/',GSTPurchaseReturn,name="purchaseReturn"),
     path('GenSaleReturn/',GSTSaleReport,name='gst-sale-return'),
+    path('GenPurchaseReturn/',GSTPurchaseReport,name='gst-purchase-return'),
 ]
--- gst/views.py
+++ gst/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from bill.models import Bill_Retailer
+from bill.models import *
 from users.views import ErrorPage
 from django.http import HttpResponse
 import csv
@@ -20,7 +20,7 @@ def GSTSaleReport(request):
         year = request.GET['year']
         query = Bill_Retailer.objects.filter(date__year=year,date__month=month)
         fields=['bill no','date','customer name','customer email','mode of payment','total bill (Rs.)']
-        filename='D:/Sale_rate-'+month+"-"+year+".csv"
+        filename='D:/Sale_GSTR-'+month+"-"+year+".csv"
         data = []
         for i in query:
             data.append([i.id,i.date,i.customer_name,i.customer_email,i.mode_of_payment,i.total_bill])
@@ -31,6 +31,28 @@ def GSTSaleReport(request):
             csvw.writerow(fields)
             csvw.writerows(data)
             
-            
+    else:       
         
-    return ErrorPage(request,"Here")
+        return ErrorPage(request,"only GET req. allowed")
+
+def GSTPurchaseReport(request):
+    if request.method=='GET':
+        month = request.GET['month']
+        year = request.GET['year']
+        query = Purchase.objects.filter(date__year=year,date__month=month)
+        fields=['bill no','date','party','mode of payment','total bill (Rs.)']
+        filename='D:/Purchase_GSTR-'+month+"-"+year+".csv"
+        data = []
+        for i in query:
+            data.append([i.id,i.date,i.party,i.mode_of_payment,i.total_bill])
+            
+
+        with open(filename,'w',newline='') as csvf:
+            csvw = csv.writer(csvf)
+            csvw.writerow(fields)
+            csvw.writerows(data)
+            
+    else:
+        return ErrorPage(request,"Only GET req. allowed")       
+        
+    
